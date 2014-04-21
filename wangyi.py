791e8c11d1201ca4a0e125e01def743d79576df5
lord63 <lord63.j@gmail.com>
2014-04-21 11:15:17 +0800

    update to v0.2

--- wangyi.py
+++ wangyi.py
@@ -7,55 +7,63 @@ import requests
 from lxml import html
 
 
-def get_picset_urls(picalbum_url):
-    page = requests.get(picalbum_url)
-    tree = html.fromstring(page.text)
-
-    picset_urls = tree.xpath('//li[@class="w-cover"]/a[@class=\
-                             "name detail f-trans j-cover"]/@href')
-    return picset_urls
+def download_photos(url):
+    if url.endswith("pp.163.com/"):
+        download_photo_collections(url)
+    else:
+        download_photo_collection(url)
 
 
-def get_urls_and_title(picset_url):
-    page = requests.get(picset_url)
-    tree = html.fromstring(page.text)
+def download_photo_collection(url):
+    photo_urls, collection_title, author = get_collection_info(url)
 
-    picture_urls = tree.xpath('//div[@class="pic-area"]/img/\
-                              @data-lazyload-src')
-    raw_picset_title = tree.xpath('//li[@class="o-box o-box-userinfo"]\
-                                  /h2[@class="picset-title"]/text()')
-    picset_title = ''.join(raw_picset_title).strip()
-    return picture_urls, picset_title
+    save_path = '/home/lord63/pictures/WangYi/' + author + "/" + collection_title
+    if os.path.exists(save_path):
+        pass
+    else:
+        os.makedirs(save_path)
 
-def make_picture_directory(picset_title):
-    picture_save_path = '/home/lord63/pictures/test/' + picset_title
-    os.mkdir(picture_save_path)
-    return picture_save_path
-    print "Successfully made the directory"
-
-
-def download_picsets(picset_urls):
-    for picset_url in picset_urls:
-        picture_urls, picset_title = get_urls_and_title(picset_url)
-
-        picture_save_path = make_picture_directory(picset_title)
-
-        download_pictures(picture_urls, picture_save_path)
-
-
-def download_pictures(picture_urls, picture_save_path):
-    for picture_url, i in zip(picture_urls, range(len(picture_urls))):
-        r = requests.get(picture_url, stream=True)
-        with open(picture_save_path+"/"+str(i)+".jpg", "wb") as f:
+    for photo_url, number in zip(photo_urls, range(len(photo_urls))):
+        image_format = '.' + photo_url.split('.')[-1]
+        picture_save_path = save_path + "/" + str(number) + image_format
+        if os.path.isfile(picture_save_path):
+            return
+        r = requests.get(photo_url, stream=True)
+        with open(picture_save_path, "wb") as f:
             for chunk in r.iter_content(1024):
                 f.write(chunk)
+    print "Successfully downloaded the collection: " + collection_title
 
 
-picalbum_url = "http://dingding147147.pp.163.com"
-picset_urls = get_picset_urls(picalbum_url)
-download_picsets(picset_urls)
+def download_photo_collections(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+    collection_urls = tree.xpath('//ul[@isshow="false"]/li/a[1]/@href')
+    print "Successfully got the collection_urls\n"
+
+    for colletion_url in collection_urls:
+        download_photo_collection(colletion_url)
+    print "\nPhoto collections have be downloaded :)"
 
 
+def get_collection_info(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+
+    photo_urls = tree.xpath('//div[@class="pic-area"]/img/@data-lazyload-src')
+
+    raw_name = tree.xpath('//li[@class="o-box o-box-userinfo"]/h2/text()')
+    name = ''.join(raw_name).strip()
+    count = tree.xpath('//li[@class="o-box o-box-userinfo"]/p[1]/b/text()')
+    collection_title = name + ''.join(count)
+
+    raw_author = tree.xpath('//p[@class="picset-author"]/a[1]/text()')
+    author = ''.join(raw_author)
+
+    return photo_urls, collection_title, author
 
 
+# ----------------------------------------------------------------------#
 
+url = raw_input("Please input the url: ")
+download_photos(url)
