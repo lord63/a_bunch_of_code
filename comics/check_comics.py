da7cc42c51429a5ea1e2fb0c501463326091fe42
lord63 <lord63.j@gmail.com>
2014-05-30 21:11:16 +0800

    Better notify for comics: use fetion to remind you

--- comics/check_comics.py
+++ comics/check_comics.py
@@ -7,6 +7,7 @@ import sys
 
 import requests
 from bs4 import BeautifulSoup
+from config import MOBILENUM, PASSWORD
 
 connect = sqlite3.connect(path.join(sys.path[0], 'comics.db'))
 cursor = connect.cursor()
@@ -20,6 +21,18 @@ def check(current_num):
     else:
         return False if cursor.fetchone() is None else True
 
+# This function is written by laomo
+# https://gist.github.com/laomo/c328834f23b26088b280#file-fetion-py
+def send_message(msg):
+    url_space_login = 'http://f.10086.cn/huc/user/space/login.do?m=submit&fr=space'
+    url_login = 'http://f.10086.cn/im/login/cklogin.action'
+    url_sendmsg = 'http://f.10086.cn/im/user/sendMsgToMyselfs.action'
+    parameter= {'mobilenum': MOBILENUM, 'password': PASSWORD}
+    session = requests.Session()
+    session.post(url_space_login, data=parameter)
+    session.get(url_login)
+    session.post(url_sendmsg, data={'msg':msg})
+
 naruto_comics = 'http://www.tvimm.com/NARUTO.html'
 r = requests.get(naruto_comics)
 soup = BeautifulSoup(r.text)
@@ -31,6 +44,7 @@ if check(num):
 else:
     print 'NARUTO: has been updated to', num
     print 'Click here--> ' + 'http://www.tvimm.com' + href
+    send_message('Naruto has been updated to ' + num)
     cursor.execute('INSERT INTO comics VALUES (?)', (num,))
     connect.commit()
 
