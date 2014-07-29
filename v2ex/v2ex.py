b725145c9c0d811866e0ed0786d3fdf5542d57b9
lord63 <lord63.j@gmail.com>
2014-07-29 16:52:52 +0800

    support arguments in terminal now

--- v2ex/v2ex.py
+++ v2ex/v2ex.py
@@ -3,12 +3,13 @@
 
 from string import maketrans
 import logging
+import os
 import sys
-from os import path
+import json
 
 import requests
 from lxml import html
-from v2ex_config import USER, PASSWD
+from terminal import Command
 
 
 def get_once(page_text):
@@ -17,16 +18,16 @@ def get_once(page_text):
     return once
 
 
-def login(url):
-    r = s.get(url, verify=False)
+def login(signin_url):
+    r = s.get(signin_url, verify=False)
     login_data = {
-        'u': USER,
-        'p': PASSWD,
+        'u': config['username'],
+        'p': config['password'],
         'once': get_once(r.text),
         'next': '/'
     }
     headers = {'Referer': 'https://www.v2ex.com/signin'}
-    s.post(url, headers=headers, data=login_data)
+    s.post(signin_url, headers=headers, data=login_data)
 
 
 def get_money(mission_url):
@@ -49,19 +50,49 @@ def get_balance(balance_url):
     tree = html.fromstring(r.text)
     total = tree.xpath('//table[@class="data"]/tr[2]/td[4]/text()')[0].strip()
     today = tree.xpath('//table[@class="data"]/tr[2]/td[5]/span/text()')[0].strip()
-    logging.info('%-26sTotal:%-13s' % (today, total))
+    logging.info('%-26sTotal:%-8s' % (today, total))
     print "Today: " + today
     print "Total: " + total
 
 
+# command parse and set
+command = Command('v2ex_daily_mission', 'complete the mission and get money')
+command.option('-u, --username [username]', 'set your username, default read from config file')
+command.option('-p, --password [password]', 'set your password, dafault read from config file')
+command.option('-l, --log_directory [direcroty]', 'set the log direcroty, default read from config file')
+
+# get config
+with open('v2ex_config.json') as f:
+    config = json.load(f)
+if command.username is not None:
+    config['username'] = command.username
+if command.password is not None:
+    config['password'] = command.password
+if command['log_directory'] is not None:
+    config['log_directory'] = command.log_directory
+
+# subcommand
+@command.action
+def read(count=5):
+    """
+    read log file
+
+    :param count: read the count of the recent days
+    :option count: -c, --count [count]
+    """
+    file_path = os.path.join(config['log_directory'], 'v2ex.log')
+    os.system('tail -n {0} {1}'.format(count, file_path))
+command.parse()
+
+# set log
 logging.basicConfig(
-    filename=path.join(sys.path[0], 'v2ex.log'),
+    filename=os.path.join(config['log_directory'], 'v2ex.log'),
     level='INFO',
     format='%(asctime)s [%(levelname)s] %(message)s')
 # Disable log message from the requests library
-# For example: Starting new HTTPS connection (1): www.v2ex.com
 requests_log = logging.getLogger("requests")
 requests_log.setLevel(logging.WARNING)
+
 
 s = requests.session()
 s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
@@ -71,5 +102,10 @@ signin_url = 'https://www.v2ex.com/signin'
 balance_url = 'https://www.v2ex.com/balance'
 mission_url = 'https://www.v2ex.com/mission/daily'
 
-login(signin_url)
-get_money(mission_url)
+def main():
+    login(signin_url)
+    get_money(mission_url)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        main()
