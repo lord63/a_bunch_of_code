bc0168a67bfd8b88c509f365bafea4289a3c0b3c
lord63 <lord63.j@gmail.com>
2014-08-10 08:35:15 +0800

    Use pandoc to keep one readme in the repo
    - update setup.py; remove the unnecessary readme.rst

--- v2ex/setup.py
+++ v2ex/setup.py
@@ -2,18 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from codecs import open
-from os import path
+import sys
 
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    from pypandoc import convert
+    readme_md = lambda f: convert(f, 'rst')
+except ImportError:
+    sys.exit("Please install pypandoc first.")
+except OSError:
+    sys.exit("You probably do not have pandoc installed.")
 
 setup(
     name='v2ex_daily_mission',
     version='0.1.0',
     description='complete mission, get money, from v2ex',
-    long_description=long_description,
+    long_description=readme_md('README.md'),
     url='https://github.com/lord63/a_bunch_of_code/tree/master/v2ex',
     author='lord63',
     author_email='lord63.j@gmail.com',
@@ -24,13 +27,14 @@ setup(
         'Operating System :: POSIX :: Linux',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         ],
     keywords='v2ex daily money sign',
     packages=['v2ex_daily_mission'],
     install_requires=['terminal','requests', 'lxml'],
     package_data={
-        'v2ex_daily_mission': ['README.rst', 'LICENSE'],
+        'v2ex_daily_mission': ['README.md', 'LICENSE'],
     },
     entry_points={
         'console_scripts':[
