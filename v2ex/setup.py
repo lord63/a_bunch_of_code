ea3b21bc5562d6782159ec6f0473f74e4f565afa
lord63 <lord63.j@gmail.com>
2014-08-17 18:52:01 +0800

    Update setup.py: to use readme in markdown

--- v2ex/setup.py
+++ v2ex/setup.py
@@ -2,21 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-import sys
 
 try:
-    from pypandoc import convert
-    readme_md = lambda f: convert(f, 'rst')
-except ImportError:
-    sys.exit("Please install pypandoc first.")
-except OSError:
-    sys.exit("You probably do not have pandoc installed.")
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    with open('README.md') as f:
+        long_description = f.read()
 
 setup(
     name='v2ex_daily_mission',
     version='0.1.0',
     description='complete mission, get money, from v2ex',
-    long_description=readme_md('README.md'),
+    long_description=long_description,
     url='https://github.com/lord63/a_bunch_of_code/tree/master/v2ex',
     author='lord63',
     author_email='lord63.j@gmail.com',
@@ -33,9 +31,7 @@ setup(
     keywords='v2ex daily money sign',
     packages=['v2ex_daily_mission'],
     install_requires=['terminal','requests', 'lxml'],
-    package_data={
-        'v2ex_daily_mission': ['README.md', 'LICENSE'],
-    },
+    include_package_data=True,
     entry_points={
         'console_scripts':[
             'v2ex_daily_mission=v2ex_daily_mission.v2ex:main']
