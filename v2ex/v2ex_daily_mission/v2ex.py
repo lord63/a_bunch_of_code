3b959c4b76c4db40d8ffd934f8a08337d04ffe85
lord63 <lord63.j@gmail.com>
2014-09-07 15:36:48 +0800

    Refine code: add docstring

--- v2ex/v2ex_daily_mission/v2ex.py
+++ v2ex/v2ex_daily_mission/v2ex.py
@@ -31,20 +31,23 @@ from terminal import Command
 
 
 # set the session and header.
-s = requests.session()
-s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
+session = requests.Session()
+session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
                    rv:28.0) Gecko/20100101 Firefox/28.0'})
 # disable urllib3 warning, see #9
 urllib3.disable_warnings()
 
+
 def get_once(page_text):
+    """get once which will be used when you login"""
     tree = html.fromstring(page_text)
     once = tree.xpath('//input[@name="once"]/@value')[0]
     return once
 
 
 def login(signin_url, config):
-    r = s.get(signin_url, verify=False)
+    """login v2ex, otherwise we can't complete the mission"""
+    r = session.get(signin_url, verify=False)
     login_data = {
         'u': config['username'],
         'p': config['password'],
@@ -52,11 +55,12 @@ def login(signin_url, config):
         'next': '/'
     }
     headers = {'Referer': 'https://www.v2ex.com/signin'}
-    s.post(signin_url, headers=headers, data=login_data)
+    session.post(signin_url, headers=headers, data=login_data)
 
 
 def get_money(mission_url, balance_url):
-    r = s.get(mission_url, verify=False)
+    """complete daily mission then get the money"""
+    r = session.get(mission_url, verify=False)
     tree = html.fromstring(r.text)
 
     raw_once = tree.xpath('//input[@type="button"]/@onclick')[0]
@@ -65,17 +69,18 @@ def get_money(mission_url, balance_url):
     if once == '/balance':
         sys.exit("You have completed the mission today.")
     else:
-        s.get('https://www.v2ex.com'+once, verify=False)
+        session.get('https://www.v2ex.com'+once, verify=False)
         get_balance(balance_url)
 
 
 def get_balance(balance_url):
-    r = s.get(balance_url, verify=False)
+    """get to know how much you totally have and how much you get today"""
+    r = session.get(balance_url, verify=False)
     tree = html.fromstring(r.text)
     total = tree.xpath('//table[@class="data"]/tr[2]/td[4]/text()')[0].strip()
     today = tree.xpath(
         '//table[@class="data"]/tr[2]/td[5]/span/text()')[0].strip()
-    logging.info('%-26sTotal:%-8s' % (today, total))
+    logging.info('%-26sTotal:%-8s', today, total)
     print "Today: {0}".format(today.encode('utf-8'))
     print "Total: {0}".format(total.encode('utf-8'))
 
@@ -93,8 +98,10 @@ def main():
         sys.exit("Don't forget your config.json.\nPlease read "
                  "https://github.com/lord63/a_bunch_of_code/tree/master/v2ex")
 
-    command = Command('v2ex_daily_mission', description='complete the mission and get money',
+    command = Command('v2ex_daily_mission',
+                      description='complete the mission and get money',
                       version=__version__)
+
     # subcommand
     @command.action
     def read(count=config['count']):
@@ -111,7 +118,7 @@ def main():
     def last():
         """how long you have kept signing in"""
         login(signin_url, config)
-        r = s.get(mission_url, verify=False)
+        r = session.get(mission_url, verify=False)
         tree = html.fromstring(r.text)
         last = tree.xpath(
             '//div[@id="Main"]/div[@class="box"]/div[3]/text()')[0].strip()
