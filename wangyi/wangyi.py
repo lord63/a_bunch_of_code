665cbc76b9f901798f0f14923993485fa1fec728
lord63 <lord63.j@gmail.com>
2015-01-29 21:40:08 +0800

    Wangyi: use session and add referer in headers

--- wangyi/wangyi.py
+++ wangyi/wangyi.py
@@ -8,13 +8,19 @@ import requests
 from bs4 import BeautifulSoup
 
 MAIN_DIR_NAME = '/home/lord63/pictures/WangYi/'
+GLOBAL_SESSION = requests.Session()
+GLOBAL_SESSION.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
+                               x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+
 
 def download_photos(given_url):
     if given_url.endswith("pp.163.com/") or given_url.endswith("pp.163.com"):
         print 'Start download photo albums...'
+        GLOBAL_SESSION.headers.update({'Referer': given_url})
         download_photo_albums(given_url)
     else:
         print 'Start download photo album...'
+        GLOBAL_SESSION.headers.update({'Referer': 'http://pp.163.com/pp/'})
         download_photo_album(given_url)
 
 
@@ -31,7 +37,7 @@ def download_photo_album(album_url):
         picture_save_path = save_path + "/" + str(number) + image_format
         if os.path.exists(picture_save_path):
             return
-        request = requests.get(photo_url, stream=True)
+        request = GLOBAL_SESSION.get(photo_url, stream=True)
         with open(picture_save_path, "wb") as f:
             for chunk in request.iter_content(1024):
                 f.write(chunk)
@@ -39,7 +45,7 @@ def download_photo_album(album_url):
 
 
 def download_photo_albums(homepage):
-    page = requests.get(homepage)
+    page = GLOBAL_SESSION.get(homepage)
     soup = BeautifulSoup(page.text)
     album_urls = []
     for album_url in soup.find_all('li', 'w-cover'):
@@ -53,7 +59,7 @@ def download_photo_albums(homepage):
 
 
 def get_album_info(album_url):
-    page = requests.get(album_url)
+    page = GLOBAL_SESSION.get(album_url)
     soup = BeautifulSoup(page.text)
 
     photo_urls = []
@@ -70,23 +76,27 @@ def get_album_info(album_url):
 
     return photo_urls, album_title, author
 
+
 def get_download_location(given_urls):
     print '\nAll the photo albums have been downloaded :)'
     photo_albums_urls = [url for url in given_urls if \
                          url.endswith("pp.163.com/") or
                          url.endswith("pp.163.com")]
-    photo_album_urls = [url for url in given_urls if url not in photo_albums_urls]
+    photo_album_urls = [
+        url for url in given_urls if url not in photo_albums_urls]
 
     if photo_albums_urls:
         for photo_albums_url in photo_albums_urls:
-            soup = BeautifulSoup(requests.get(photo_albums_url).text)
+            soup = BeautifulSoup(GLOBAL_SESSION.get(photo_albums_url).text)
             author = soup.find('h2', 'host-nname').a['title'].encode('utf-8')
-            print 'Have a check your albums in {}{}/'.format(MAIN_DIR_NAME, author)
+            print 'Have a check your albums in {}{}/'.format(MAIN_DIR_NAME,
+                                                             author)
     if photo_album_urls:
         for photo_album_url in photo_album_urls:
             photo_urls, album_title, author = get_album_info(photo_album_url)
             print 'Your single album in {}{}/{}'.format(
                 MAIN_DIR_NAME, author, album_title)
+
 
 if __name__ == '__main__':
     urls = sys.argv[1:]
